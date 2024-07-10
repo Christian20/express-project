7b29f7746d43943aa923fbe95af08b68defbdc4d
Christian Steia <christian_steia@yahoo.com>
2024-07-10 15:34:24 +0300

    added views

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,5 +1,11 @@
+const path = require('path');
+
 function getMessages(req, res) {
-  res.send('<ul><li>Hello Albert!</li></ul>');
+  res.render('messages', {
+    title: 'Messages to my Friends',
+    friend: 'Elon Musk',
+  });
+  // res.sendFile(path.join(__dirname, '..', 'public', 'images', 'skimountain.jpg'));
 }
 
 function postMessage(req, res) {
